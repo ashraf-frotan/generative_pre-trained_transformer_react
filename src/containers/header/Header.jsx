b5127ba3d4cb30d89618058132b6d9165a410d0d
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2023-04-19 00:18:24 +0430

    Navbar done, Header(60%) done

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './header.css'
+import people from "../../assets/people.png"
+import ai from "../../assets/ai.png"
+const Header=()=>{
+    return (
+        <div className='gpt3__header section__padding' id='home'>
+            <div className='gpt3__header-content'>
+                <h1 className='gradient__text'>
+                    Let's Build Something amazing with GPT-3 OpenAI
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo delectus molestias? Velit, beatae quos. Vel quos rem molestiae omnis optio ad nostrum consequuntur, praesentium, distinctio tempora aut sequi ipsum?
+                </p>
+                <div className='gpt3__header-content__input'>
+                    <input type="email" placeholder='You email address' />
+                    <button type='button'>Get Started</button>
+                </div>
+                <div className='gpt3__header-content__people'>
+                    <img src={people} alt="" />
+                    <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+                <div className='gpt__header-image'>
+                    <img src={ai} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
